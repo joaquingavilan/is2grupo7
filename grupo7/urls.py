e61d736f906cfc6191c688e291debee8a7ba5ec5
joaquingavilan <joaquinrgavilan@gmail.com>
2022-08-14 16:02:30 -0400

    creacion del login

--- grupo7/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import hola
-
-urlpatterns = [
-    path('hola/', hola, name="hola"),
-]
--- grupo7/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def hola(request):
-    return HttpResponse("Hola Mundo Kp")
