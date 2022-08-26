faa62e02de78d703cc93e3d0355c4979d255a9bc
joaquingavilan <joaquinrgavilan@gmail.com>
2022-08-26 01:18:15 -0400

    diseño de pantalla de modificacion de usuario

--- ings2/proyecto/urls.py
+++ ings2/proyecto/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from gestionProyectos import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("gestionProyectos.urls")),
+    path('mod_usuario', views.mod_usuario)
 ]
